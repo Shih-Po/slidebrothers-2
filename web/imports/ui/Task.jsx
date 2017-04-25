43eb27308bf11b0a10241a84764c0a4535c7ff92
max80713 <max80713@hotmail.com>
2017-04-26 02:42:32 +0800

    Replace proptypes using npm packages

--- web/imports/ui/Task.jsx
+++ web/imports/ui/Task.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Tasks from '../api/tasks';
 
 // Task component - represents a single todo item
