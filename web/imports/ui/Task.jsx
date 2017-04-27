0e77da5c87f8dc41a8a2db1e089efc3c386ee296
max80713 <max80713@hotmail.com>
2017-04-28 00:44:59 +0800

    Check show private button prop types

--- web/imports/ui/Task.jsx
+++ web/imports/ui/Task.jsx
@@ -62,4 +62,5 @@ Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
   task: PropTypes.object.isRequired,
+  showPrivateButton: PropTypes.bool.isRequired,
 };
