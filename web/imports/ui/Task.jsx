7649bd9fd866bb30fb2d6aa70ab91cd4005caf9e
max80713 <max80713@hotmail.com>
2017-04-27 23:55:41 +0800

    Add npm classnames package

--- web/imports/ui/Task.jsx
+++ web/imports/ui/Task.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 // Task component - represents a single todo item
 export default class Task extends Component {
