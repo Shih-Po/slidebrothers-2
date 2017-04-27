47cde9983824615bd7eee03580c55d50c6f8aac7
max80713 <max80713@hotmail.com>
2017-04-28 00:45:45 +0800

    Set show private button prop

--- web/imports/ui/App.jsx
+++ web/imports/ui/App.jsx
@@ -42,9 +42,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map(task => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser ? this.props.currentUser._id : null;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
 
   render() {
