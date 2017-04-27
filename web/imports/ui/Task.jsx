245abaed4a56b5d9b03299f35663f313380aff85
max80713 <max80713@hotmail.com>
2017-04-28 00:45:13 +0800

    Add class names

--- web/imports/ui/Task.jsx
+++ web/imports/ui/Task.jsx
@@ -28,7 +28,10 @@ export default class Task extends Component {
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
-    const taskClassName = this.props.task.checked ? 'checked' : '';
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
 
     return (
       <li className={taskClassName}>
