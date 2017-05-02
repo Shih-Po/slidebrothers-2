c2797c6a317375c09afced5e25eaa11d27550860
Max Hsu <maxh@arisan.io>
2017-05-02 12:55:25 +0800

    Correct indent

--- web/imports/ui/CasePtt.jsx
+++ web/imports/ui/CasePtt.jsx
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// Task component - represents a single todo item
 export default class CasePtt extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <li className="case-ptt">
-                <label className="text">
-                    <strong>{this.props.casePtt.title}</strong>
-                </label>
-            </li>
-        );
-    }
+  render() {
+    return (
+      <li className="case-ptt">
+        <label className="text">
+          <strong>{this.props.casePtt.title}</strong>
+        </label>
+      </li>
+    );
+  }
 }
 
 CasePtt.propTypes = {
-    // This component gets the task to display through a React prop.
-    // We can use propTypes to indicate it is required
-    casePtt: PropTypes.object.isRequired,
+  casePtt: PropTypes.object.isRequired,
 };
