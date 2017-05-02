1e93d2905bf91d5e3d12df77283e223d1938c9ba
Max Hsu <maxh@arisan.io>
2017-05-02 12:54:57 +0800

    Render cases of 104 and 518

--- web/imports/ui/App.jsx
+++ web/imports/ui/App.jsx
@@ -3,10 +3,14 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
+import Cases104 from '../api/cases_104/collections';
+import Cases518 from '../api/cases_518/collections';
 import CasesPtt from '../api/cases_ptt/collections';
 import Tasks from '../api/tasks';
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
+import Case104 from './Case104.jsx';
+import Case518 from './Case518.jsx';
 import CasePtt from './CasePtt.jsx';
 import Task from './Task.jsx';
 
@@ -58,6 +62,18 @@ class App extends Component {
     });
   }
 
+  renderCases104() {
+    return this.props.cases104.map(case104 => (
+        <Case104 key={case104._id} case104={case104} />
+    ))
+  }
+
+  renderCases518() {
+    return this.props.cases518.map(case518 => (
+        <Case518 key={case518._id} case518={case518} />
+    ))
+  }
+
   renderCasesPtt() {
     return this.props.casesPtt.map(casePtt => (
         <CasePtt key={casePtt._id} casePtt={casePtt} />
@@ -101,16 +117,38 @@ class App extends Component {
           {/*{this.renderTasks()}*/}
         {/*</ul>*/}
 
-        <ul>
-          {this.renderCasesPtt()}
-        </ul>
+        <div className="row">
+          <div className="col s4">
+            <li>104 ({this.props.cases104Count})</li>
+            <ul>
+              {this.renderCases104()}
+            </ul>
+          </div>
+          <div className="col s4">
+            <li>518 ({this.props.cases518Count})</li>
+            <ul>
+              {this.renderCases518()}
+            </ul>
+          </div>
+          <div className="col s4">
+            <li>Ptt ({this.props.casesPttCount})</li>
+            <ul>
+              {this.renderCasesPtt()}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 App.propTypes = {
+  cases104: PropTypes.array.isRequired,
+  cases518: PropTypes.array.isRequired,
   casesPtt: PropTypes.array.isRequired,
+  cases104Count: PropTypes.number.isRequired,
+  cases518Count: PropTypes.number.isRequired,
+  casesPttCount: PropTypes.number.isRequired,
   tasks: PropTypes.array.isRequired,
   incompleteCount: PropTypes.number.isRequired,
   currentUser: PropTypes.object,
@@ -118,10 +156,17 @@ App.propTypes = {
 
 export default createContainer(() => {
   Meteor.subscribe('tasks');
+  Meteor.subscribe('case_104');
+  Meteor.subscribe('case_518');
   Meteor.subscribe('case_ptt');
 
   return {
-    casesPtt: CasesPtt.find().fetch(),
+    cases104: Cases104.find({}, { limit: 50 }).fetch(),
+    cases518: Cases518.find({}, { limit: 50 }).fetch(),
+    casesPtt: CasesPtt.find({}, { limit: 50 }).fetch(),
+    cases104Count: Cases104.find().count(),
+    cases518Count: Cases518.find().count(),
+    casesPttCount: CasesPtt.find().count(),
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     currentUser: Meteor.user(),
