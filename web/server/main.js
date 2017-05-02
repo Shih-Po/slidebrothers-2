e024f712e9c769041d1f107c5fa694bde09b0822
Max Hsu <maxh@arisan.io>
2017-05-02 12:21:22 +0800

    Import publication of case_ptt collection

--- web/server/main.js
+++ web/server/main.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 import Tasks from '../imports/api/tasks';
+import CasesPtt from '../imports/api/cases_ptt/server/publications';
 
 Meteor.startup(() => {
   // code to run on server at startup
