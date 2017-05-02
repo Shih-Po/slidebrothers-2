cff98734170992a852e63f4afbaf263e4d805c3a
Max Hsu <maxh@arisan.io>
2017-05-02 12:55:35 +0800

    Import publications

--- web/server/main.js
+++ web/server/main.js
@@ -1,6 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
 import Tasks from '../imports/api/tasks';
+import Cases104 from '../imports/api/cases_104/server/publications';
+import Cases518 from '../imports/api/cases_518/server/publications';
 import CasesPtt from '../imports/api/cases_ptt/server/publications';
 
 Meteor.startup(() => {
