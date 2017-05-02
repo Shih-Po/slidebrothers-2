f75bb244f9c329fef0ad260c926b3f665a79411e
Max Hsu <maxh@arisan.io>
2017-05-02 12:55:13 +0800

    Add Case104 and  Case518 jsx

---
+++ web/imports/ui/Case104.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Case104 extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <li className="case-104">
+        <label className="text">
+          <strong>{this.props.case104.title}</strong>
+        </label>
+      </li>
+    );
+  }
+}
+
+Case104.propTypes = {
+  case104: PropTypes.object.isRequired,
+};
---
+++ web/imports/ui/Case518.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Case518 extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <li className="case-518">
+        <label className="text">
+          <strong>{this.props.case518.title}</strong>
+        </label>
+      </li>
+    );
+  }
+}
+
+Case518.propTypes = {
+  case518: PropTypes.object.isRequired,
+};
