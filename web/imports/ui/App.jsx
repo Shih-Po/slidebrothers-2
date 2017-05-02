2f2fdbe8f976d4cd54d4b9d932f5f2a42a9e4f44
Max Hsu <maxh@arisan.io>
2017-05-02 14:48:29 +0800

    Show cases sorted by id descendingly

--- web/imports/ui/App.jsx
+++ web/imports/ui/App.jsx
@@ -171,9 +171,9 @@ export default createContainer(() => {
   Meteor.subscribe('case_ptt');
 
   return {
-    cases104: Cases104.find({}, { limit: 50 }).fetch(),
-    cases518: Cases518.find({}, { limit: 50 }).fetch(),
-    casesPtt: CasesPtt.find({}, { limit: 50 }).fetch(),
+    cases104: Cases104.find({}, { limit: 50, sort: { _id: -1 } }).fetch(),
+    cases518: Cases518.find({}, { limit: 50, sort: { _id: -1 } }).fetch(),
+    casesPtt: CasesPtt.find({}, { limit: 50, sort: { _id: -1 } }).fetch(),
     cases104Count: Cases104.find().count(),
     cases518Count: Cases518.find().count(),
     casesPttCount: CasesPtt.find().count(),
