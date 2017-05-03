8d45a1df0e506ba28bc11c62a088567584527141
max80713 <max80713@hotmail.com>
2017-05-04 01:45:08 +0800

    Remove unused code

--- web/imports/ui/App.jsx
+++ web/imports/ui/App.jsx
@@ -6,70 +6,31 @@ import { createContainer } from 'meteor/react-meteor-data';
 import Cases104 from '../api/cases_104/collections';
 import Cases518 from '../api/cases_518/collections';
 import CasesPtt from '../api/cases_ptt/collections';
-import Tasks from '../api/tasks';
 
 import Case104 from './Case104.jsx';
 import Case518 from './Case518.jsx';
 import CasePtt from './CasePtt.jsx';
-import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleKeyUp = this.handleKeyUp.bind(this);
     this.state = {
       hideCompleted: false,
       keyword: '',
     };
-    this.toggleHideCompleted = this.toggleHideCompleted.bind(this);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-
-    // Find the text field via the React ref
-    const text = this.textInput.value.trim();
-
-    Meteor.call('tasks.insert', text);
-
-    // Clear form
-    this.textInput.value = '';
-  }
-
-  handleKeyUp(event) {
     event.preventDefault();
 
     const text = this.textInput.value.trim();
+
+
+
 
     this.setState({
       keyword: text,
-    });
-  }
-
-  toggleHideCompleted() {
-    this.setState({
-      hideCompleted: !this.state.hideCompleted,
-    });
-  }
-
-  renderTasks() {
-    let filteredTasks = this.props.tasks;
-    if (this.state.hideCompleted) {
-      filteredTasks = filteredTasks.filter(task => !task.checked);
-    }
-    return filteredTasks.map((task) => {
-      const currentUserId = this.props.currentUser ? this.props.currentUser._id : null;
-      const showPrivateButton = task.owner === currentUserId;
-
-      return (
-        <Task
-          key={task._id}
-          task={task}
-          showPrivateButton={showPrivateButton}
-        />
-      );
     });
   }
 
@@ -109,33 +70,6 @@ class App extends Component {
             <label htmlFor="input-text">搜尋案件</label>
           </div>
         </div>
-        {/*<input*/}
-          {/*type="checkbox"*/}
-          {/*className="filled-in"*/}
-          {/*id="checkbox-hide-completed"*/}
-          {/*readOnly*/}
-          {/*checked={this.state.hideCompleted}*/}
-          {/*onClick={this.toggleHideCompleted}*/}
-        {/*/>*/}
-        {/*<label className="hide-completed" htmlFor="checkbox-hide-completed">*/}
-          {/*Hide Completed Tasks*/}
-        {/*</label>*/}
-
-        {/*<AccountsUIWrapper />*/}
-
-        {/*{ this.props.currentUser ?*/}
-          {/*<form className="new-task" onSubmit={this.handleSubmit} >*/}
-            {/*<input*/}
-              {/*type="text"*/}
-              {/*ref={(c) => { this.textInput = c; }}*/}
-              {/*placeholder="Type to add new tasks"*/}
-            {/*/>*/}
-          {/*</form> : ''*/}
-        {/*}*/}
-
-        {/*<ul>*/}
-          {/*{this.renderTasks()}*/}
-        {/*</ul>*/}
 
         <div className="row">
           <div className="col s4">
@@ -169,8 +103,6 @@ App.propTypes = {
   cases104Count: PropTypes.number.isRequired,
   cases518Count: PropTypes.number.isRequired,
   casesPttCount: PropTypes.number.isRequired,
-  tasks: PropTypes.array.isRequired,
-  currentUser: PropTypes.object,
 };
 
 export default createContainer(() => {
@@ -186,8 +118,5 @@ export default createContainer(() => {
     cases104Count: Cases104.find().count(),
     cases518Count: Cases518.find().count(),
     casesPttCount: CasesPtt.find().count(),
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
-    currentUser: Meteor.user(),
   };
 }, App);
