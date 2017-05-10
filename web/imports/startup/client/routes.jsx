1d4b7ff48443b6ec6559abda95acd316850b71b3
max80713 <max80713@hotmail.com>
2017-05-10 23:37:49 +0800

    Import case page and mount onto route

--- web/imports/startup/client/routes.jsx
+++ web/imports/startup/client/routes.jsx
@@ -2,6 +2,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import React from 'react';
 import { mount } from 'react-mounter';
 
+import CasePage from '../../ui/CasePage.jsx';
 import MainLayout from '../../ui/MainLayout.jsx';
 import SearchPage from '../../ui/SearchPage.jsx';
 
@@ -13,11 +14,11 @@ FlowRouter.route('/', {
   },
 });
 
-FlowRouter.route('/case/:_id', {
+FlowRouter.route('/case/:type/:_id', {
   name: 'case',
   action: (params) => {
     mount(MainLayout, {
-      content: <CasePage _id={params._id} />
+      content: <CasePage _id={params._id} type={params.type} />,
     });
   },
 });
