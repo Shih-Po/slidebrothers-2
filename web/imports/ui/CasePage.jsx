643a0bcf74c05f7c3a9b96640e09638e19332b76
max80713 <max80713@hotmail.com>
2017-05-10 23:38:23 +0800

    Add case page

---
+++ web/imports/ui/CasePage.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Mongo } from 'meteor/mongo';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import Cases104 from '../api/cases_104/collections';
+import Cases518 from '../api/cases_518/collections';
+import CasesPtt from '../api/cases_ptt/collections';
+
+class CasePage extends Component {
+  renderDetail() {
+    return Object.entries(this.props.aCase).map(entry => <div>{entry[0]}</div>);
+  }
+
+  render() {
+    return (
+      <div className="case-page">
+        {this.renderDetail()}
+      </div>
+    );
+  }
+}
+
+CasePage.propTypes = {
+  aCase: PropTypes.shape().isRequired,
+};
+
+export default createContainer(({ _id, type }) => {
+  const props = {};
+  switch (type) {
+    case '104':
+      props.aCase = Cases104.findOne(new Mongo.ObjectID(_id));
+      break;
+    case '518':
+      props.aCase = Cases518.findOne(_id);
+      break;
+    case 'ptt':
+      props.aCase = CasesPtt.findOne(_id);
+      break;
+    default:
+      break;
+  }
+  return props;
+}, CasePage);
