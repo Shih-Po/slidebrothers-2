6b62f4152fc5bbd39648fda437c3319d984d2cf7
Max Hsu <maxh@arisan.io>
2017-05-28 13:43:13 +0800

    Add case component

---
+++ web/imports/ui/Case.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Case = ({ aCase }) => {
+  const source = aCase.source;
+  let color;
+  if (source === '104') color = 'orange-text';
+  else if (source === '518') color = 'green-text';
+  else color = 'purple-text';
+  return (
+    <a
+      className={`collection-item ${color}`}
+      href={`/case/${aCase.source}/${aCase._id}`}
+    >
+      {aCase.title}
+    </a>
+  );
+};
+
+// noinspection JSUnresolvedFunction
+Case.propTypes = {
+  aCase: PropTypes.shape().isRequired,
+};
+
+export default Case;
