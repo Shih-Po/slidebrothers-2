118de13868b7ffbeb482c70de128ea616d1be8cd
Max Hsu <maxh@arisan.io>
2017-05-28 13:42:32 +0800

    Render cases sorted by post date from merged collection

--- web/imports/ui/SearchPage.jsx
+++ web/imports/ui/SearchPage.jsx
@@ -16,9 +16,24 @@ class SearchPage extends React.Component {
     this.state = {
       keyword: '',
       cases104Count: 0,
+      cases104TotalCount: 0,
       cases518Count: 0,
+      cases518TotalCount: 0,
       casesPttCount: 0,
+      casesPttTotalCount: 0,
+      filteredCases: [],
     };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const cases = nextProps.cases;
+
+    this.setState({
+      cases104TotalCount: cases.filter(aCase => aCase.source === '104').length,
+      cases518TotalCount: cases.filter(aCase => aCase.source === '518').length,
+      casesPttTotalCount: cases.filter(aCase => aCase.source === 'ptt').length,
+      filteredCases: cases,
+    });
   }
 
   handleKeyUp(event) {
@@ -26,48 +41,21 @@ class SearchPage extends React.Component {
 
     const text = this.textInput.value.trim();
 
-    const cases104 = this.props.cases104.filter(
-      case104 => (case104.title && case104.title.includes(text)) || case104.content.includes(text),
-    );
-
-    const cases518 = this.props.cases518.filter(
-      case518 => (case518.title && case518.title.includes(text)) || case518.content.includes(text),
-    );
-
-    const casesPtt = this.props.casesPtt.filter(
-      casePtt => (casePtt.title && casePtt.title.includes(text)) || casePtt.content.includes(text),
+    const cases = this.props.cases.filter(
+      aCase => (aCase.title && aCase.title.includes(text)) || aCase.content.includes(text),
     );
 
     this.setState({
-      cases104Count: cases104.length,
-      cases518Count: cases518.length,
-      casesPttCount: casesPtt.length,
-      keyword: text,
+      cases104Count: cases.filter(aCase => aCase.source === '104').length,
+      cases518Count: cases.filter(aCase => aCase.source === '518').length,
+      casesPttCount: cases.filter(aCase => aCase.source === 'ptt').length,
+      filteredCases: cases,
     });
   }
 
-  renderCases104() {
-    const keyword = this.state.keyword;
-    return this.props.cases104
-      .filter(case104 => (case104.title && case104.title.includes(keyword)) || case104.content.includes(keyword))
-      .slice(0, 50)
-      .map(case104 => (<Case104 key={case104._id} case104={case104} />));
-  }
-
-  renderCases518() {
-    const keyword = this.state.keyword;
-    return this.props.cases518
-      .filter(case518 => (case518.title && case518.title.includes(keyword)) || case518.content.includes(keyword))
-      .slice(0, 50)
-      .map(case518 => (<Case518 key={case518._id} case518={case518} />));
-  }
-
-  renderCasesPtt() {
-    const keyword = this.state.keyword;
-    return this.props.casesPtt
-      .filter(casePtt => (casePtt.title && casePtt.title.includes(keyword)) || casePtt.content.includes(keyword))
-      .slice(0, 50)
-      .map(casePtt => (<CasePtt key={casePtt._id} casePtt={casePtt} />));
+  renderCases() {
+    return this.state.filteredCases.slice(0, 50)
+      .map(aCase => (<Case key={aCase._id} aCase={aCase} />));
   }
 
   render() {
@@ -95,31 +83,7 @@ class SearchPage extends React.Component {
               }/{this.props.cases104TotalCount})
             </div>
             <div className="collection">
-              {this.renderCases104()}
-            </div>
-          </div>
-          <div className="col s4">
-            <div>
-              518 ({
-                this.state.keyword ?
-                this.state.cases518Count :
-                this.props.cases518TotalCount
-              }/{this.props.cases518TotalCount})
-            </div>
-            <div className="collection">
-              {this.renderCases518()}
-            </div>
-          </div>
-          <div className="col s4">
-            <div>
-              Ptt ({
-                this.state.keyword ?
-                this.state.casesPttCount :
-                this.props.casesPttTotalCount
-                }/{this.props.casesPttTotalCount})
-            </div>
-            <div className="collection">
-              {this.renderCasesPtt()}
+              {this.renderCases()}
             </div>
           </div>
         </div>
@@ -135,16 +99,9 @@ SearchPage.propTypes = {
 
 
 export default createContainer(() => {
-  subsManager.subscribe('case_104');
-  subsManager.subscribe('case_518');
-  subsManager.subscribe('case_ptt');
+  subsManager.subscribe('cases');
 
-  return {
-    cases104: Cases104.find({}, { sort: { _id: -1 } }).fetch(),
-    cases518: Cases518.find({}, { sort: { _id: -1 } }).fetch(),
-    casesPtt: CasesPtt.find({}, { sort: { _id: -1 } }).fetch(),
-    cases104TotalCount: Cases104.find().count(),
-    cases518TotalCount: Cases518.find().count(),
-    casesPttTotalCount: CasesPtt.find().count(),
-  };
+  return subsManager.ready() ? {
+    cases: Cases.find({}, { sort: { post_date: -1 } }).fetch(),
+  } : { cases: [] };
 }, SearchPage);
